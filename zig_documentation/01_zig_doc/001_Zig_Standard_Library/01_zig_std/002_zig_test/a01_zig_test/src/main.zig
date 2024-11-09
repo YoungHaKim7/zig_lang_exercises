213625d3fb3b8f6ebcbd5b1215cadd196aee8151
GYoung <ytok1108@aol.com>
2024-11-09 22:58:13 +0900

    zig test src/main.zig

--- zig_documentation/01_zig_doc/001_Zig_Standard_Library/01_zig_std/002_zig_test/a01_zig_test/src/main.zig
+++ zig_documentation/01_zig_doc/001_Zig_Standard_Library/01_zig_std/002_zig_test/a01_zig_test/src/main.zig
@@ -16,7 +16,12 @@ pub fn main() !void {
     try bw.flush(); // don't forget to flush!
 }
 
-test "simple test" {}
+test "simple test" {
+    var list = std.ArrayList(i32).init(std.testing.allocator);
+    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
+    try list.append(42);
+    try std.testing.expectEqual(@as(i32, 42), list.pop());
+}
 
 test "expect addOne adds one to 41" {
 
